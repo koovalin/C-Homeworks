964ab2af41492dd120ac1562e9d1615aaffb3a47
Ivan <koovalin@gmail.com>
2022-11-17 19:03:43 +0300

    add Задача 8

---
+++ Задача 8/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 8
+
+Console.WriteLine("Введи положительное число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+if (num<0) num = -num;
+int count = 1;
+
+while (count<=num)
+{
+    Console.Write($"{count}, ");
+    count ++;
+}
