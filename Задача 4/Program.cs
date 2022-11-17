8deb5c6059c1a791cf6bc19085e14a8b5ca79c0d
Ivan <koovalin@gmail.com>
2022-11-17 18:44:44 +0300

    add Задача 4

---
+++ Задача 4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 4
+Console.WriteLine("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите третье число: ");
+int num3 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Вы ввели числа {num1}, {num2}, {num3}.");
+
+int max_num = num1;
+if (max_num < num2) max_num = num2;
+if (max_num < num3) max_num = num3;
+
+Console.WriteLine($"Максимальное число = {max_num}");
