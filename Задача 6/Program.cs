68835ab12d13991b25495a0828aedb40d3fcf716
Ivan <koovalin@gmail.com>
2022-11-17 18:53:32 +0300

    add Задача 6

---
+++ Задача 6/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 6
+
+Console.WriteLine("Введите число:");
+int num = int.Parse(Console.ReadLine()!);
+
+if (num % 2 == 0)
+{
+    Console.WriteLine($"ДА. Число {num} делится на 2 без остатка!");
+}
+else
+{
+    Console.WriteLine($"НЕТ. Число {num} НЕ делится на 2 без остатка!");
+}
+
+
