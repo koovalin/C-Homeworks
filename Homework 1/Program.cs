44d361cc72db5400c6893c428ba9d85e73bb3a91
Ivan <koovalin@gmail.com>
2022-11-17 18:10:53 +0300

    add Homework 1

--- Homework 1/Program.cs
+++ Homework 1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача №2
+
+
+Console.WriteLine("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine()!);
+
+if (num1 == num2) Console.WriteLine("Числа равны!");
+if (num1 > num2){
+    Console.WriteLine($"Первое число {num1} больше второго числа {num2}");
+}
+else{
+    Console.WriteLine($"Второе число {num2} больше первого числа {num1}");
+}
