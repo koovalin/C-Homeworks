aace4664f24c2ac5014b67a0331ecc6fd6a05be2
Ivan <koovalin@gmail.com>
2022-11-26 12:19:55 +0300

    modified

--- Задача 25/Program.cs
+++ Задача 25/Program.cs
@@ -1,21 +1,30 @@
-﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
-// (задача стоит в том, чтобы написать цикл)
+﻿namespace Example25
+{
+    class Example{
+        static void Main(string[] args)
+        {
+            // Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+            // (задача стоит в том, чтобы написать цикл)
 
-Console.WriteLine("Введите число:");
-int A = int.Parse(Console.ReadLine()!);
-Console.WriteLine("Введите степень число:");
-int B = int.Parse(Console.ReadLine()!);
-Console.WriteLine($"{Stepen(A, B)}");
+            Console.WriteLine("Введите число:");
+            int A = int.Parse(Console.ReadLine()!);
+            Console.WriteLine("Введите степень число:");
+            int B = int.Parse(Console.ReadLine()!);
+            Console.WriteLine($"{Stepen(A, B)}");
 
 
-// _____________________ Методы _____________________
+            // _____________________ Методы _____________________
 
-int Stepen(int a, int b){
-    int temp = a;
-    for (int i = 0; i < b-1; i++)
-    {
-        a *= temp;
+            int Stepen(int a, int b){
+                int temp = a;
+                for (int i = 0; i < b-1; i++)
+                {
+                    a *= temp;
+                }
+                return a;
+            }
+
+            Console.ReadKey();
+        }
     }
-    return a;
 }
-
