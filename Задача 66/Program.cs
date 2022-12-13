bc49d3fa52fd5e15da3fc1a60ffc7cdec8513a8b
Ivan <koovalin@gmail.com>
2022-12-13 20:33:05 +0300

    add 64,66,68

---
+++ Задача 66/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+class Program
+{
+    static int SumElem(int start, int end)
+    {
+        if (start > end){
+            int tmp = start;
+            start = end;
+            end = tmp;
+        }
+        if (start == end) return start;
+        return (start + SumElem(start + 1, end));
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Введите M: ");
+        int M = int.Parse(Console.ReadLine()!);
+        Console.Write("Введите N: ");
+        int N = int.Parse(Console.ReadLine()!);
+        Console.WriteLine(SumElem(M, N));
+    }
+}
---
+++ Задача 68/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+class Program
+{
+    static int A(int n, int m) // Ackermann Function
+    {
+        if (n == 0)
+            return m + 1;
+        else
+            if ((n != 0) && (m == 0))
+            return A(n - 1, 1);
+        else
+            return A(n - 1, A(n, m - 1));
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine(A(2, 3)); // 9
+        Console.WriteLine(A(3, 2)); // 29
+
+    }
+}
